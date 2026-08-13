9c1a6ff34623f6043ffa8fd223b572b5e00418bd
Cristian Palacio <cristiancamilopala@gmail.com>
2026-08-12 22:25:29 -0500

    feat: add health check endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
